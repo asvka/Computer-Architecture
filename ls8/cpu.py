2af44361f0800326c319704e083329cd7897d815
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-09-11 18:40:34 -0700

    Cleaned up code, removed original messy solution

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,21 +1,7 @@
-"""CPU functionality."""
-
 import sys
-# LDI = 0b10000010
-# PRN = 0b01000111
-# HLT = 0b00000001
-# MUL = 0b10100010
-# PUSH = 0b01000101
-# POP = 0b01000110
-# RET = 0b00010001
-# CALL = 0b01010000
-# ADD = 0b10100000
-
 
 
 class CPU:
-    """Main CPU class."""
-
     def __init__(self):
         self.ram = [0] * 256
         self.reg = [0] * 8
@@ -38,8 +24,6 @@ class CPU:
             0b01010110: self.JNE,
             0b01010101: self.JEQ,
         }
-
-    # Implement the load() function to load an .ls8 file given the filename passed in as an argument
 
     def LDI(self):
         operand_a = self.ram_read(self.pc + 1)
@@ -114,16 +98,11 @@ class CPU:
         else:
             self.pc += 2
 
-
     def load(self):
         address = 0
-        # get by filename
-        print(sys.argv)
-        # fail safe
         if len(sys.argv) != 2:
             print(f'Usage: {sys.argv} filename')
             sys.exit(1)
-
         try:
             with open(sys.argv[1]) as f:
                 for line in f:
@@ -140,17 +119,12 @@ class CPU:
             sys.exit(2)
 
     def ram_read(self, MAR):
-        # ram_read() should accept the address to read and return the value stored there.
-        # our CPU has a total of 256 bytes of memory and can only compute values up to 255
         return self.ram[MAR]
 
     def ram_write(self, MAR, MDR):
-        # ram_write() should accept a value to write, and the address to write it to.
         self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "SUB":
@@ -170,11 +144,6 @@ class CPU:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -190,60 +159,8 @@ class CPU:
         print()
 
     def run(self):
-        # Binary codes for LDI, PRN, and HLT
-        # while running is true
         while self.running:
             IR = self.ram_read(self.pc)
 
             if IR in self.branch_table:
                 self.branch_table[IR]()
-            # instruction = self.ram[self.pc]
-            # sp = self.reg[7]
-            # sp &= 0xff
-            # # self.reg[sp] = 244
-            # operand_a = self.ram_read(self.pc + 1)
-            # operand_b = self.ram_read(self.pc + 2)
-            # # if self.ram == HLT (base case kinda?)
-            # # if instruction == HLT:
-            # #     self.running = False
-            # #     self.pc += 1
-            # # if self.ram == LDI
-            # elif instruction == LDI:
-            #     self.reg[operand_a] = operand_b
-            #     self.pc += 3
-            # # if self.ram == PRN
-            # elif instruction == PRN:
-            #     print(self.reg[operand_a])
-            #     self.pc += 2
-            # elif instruction == MUL:
-            #     self.alu('MUL', operand_a, operand_b)
-            #     self.pc += 3
-            #
-            # elif instruction == ADD:
-            #     self.alu('ADD', operand_a, operand_b)
-            #     self.pc += 3
-            #
-            # elif instruction == PUSH:
-            #     reg_index = self.ram_read(self.pc + 1)
-            #     self.reg[self.sp] -= 1
-            #     self.ram_write(self.reg[self.sp], self.reg[reg_index])
-            #     self.pc += 2
-            #
-            # elif instruction == POP:
-            #     reg_index = self.ram_read(self.pc + 1)
-            #     self.reg[reg_index] = self.ram_read(self.reg[self.sp])
-            #     self.reg[self.sp] += 1
-            #     self.pc += 2
-            #
-            # elif instruction == CALL:
-            #     given_reg = self.ram[self.pc + 1]
-            #     self.reg[7] -= 1
-            #     self.ram[self.reg[7]] = self.pc + 2
-            #     self.pc = self.reg[given_reg]
-            #
-            # elif instruction == RET:
-            #     # self.pc = self.ram_read(self.sp)
-            #     # self.sp += 1
-            #     stack_p = self.reg[7]
-            #     self.pc = self.ram[stack_p]
-            #     self.reg[7] += 1
